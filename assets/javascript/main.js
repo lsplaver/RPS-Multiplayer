303438af1d1d7c4ef88e0aefdf90fe4d00114474
unknown <lsplaver@yahoo.com>
2018-03-16 21:28:41 -0400

    Update 1

---
+++ assets/javascript/main.js
@@ -0,0 +1,10 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBKKI1ret6b6SjDya6hubo4AqPGwk0ajq8",
+    authDomain: "rps-multiplayer-splaver.firebaseapp.com",
+    databaseURL: "https://rps-multiplayer-splaver.firebaseio.com",
+    projectId: "rps-multiplayer-splaver",
+    storageBucket: "",
+    messagingSenderId: "115898659707"
+  };
+  firebase.initializeApp(config);
